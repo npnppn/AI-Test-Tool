b2657af774ad7f6abafa508b02b468d0bd43b0a7
Doyeop Kwon <kwon32220@gmail.com>
2021-11-10 14:17:40 +0900

    S05P31F004-92 [ADD] : IoU Score 추가

--- AI/custom/pytorch-unet-master/train.py
+++ AI/custom/pytorch-unet-master/train.py
@@ -1,4 +1,4 @@
-## 라이브러리 추가하기
+# 라이브러리 추가하기
 import argparse
 
 import os
@@ -17,11 +17,14 @@ import matplotlib.pyplot as plt
 
 from torchvision import transforms, datasets
 
+from iou import *
+
+
 import gc
 gc.collect()
 torch.cuda.empty_cache()
 
-## Parser 생성하기
+# Parser 생성하기
 parser = argparse.ArgumentParser(description="Train the UNet",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -29,30 +32,35 @@ parser.add_argument("--lr", default=1e-3, type=float, dest="lr")
 parser.add_argument("--batch_size", default=4, type=int, dest="batch_size")
 parser.add_argument("--num_epoch", default=100, type=int, dest="num_epoch")
 
-parser.add_argument("--data_dir", default="./datasets", type=str, dest="data_dir")
-parser.add_argument("--ckpt_dir", default="./checkpoint", type=str, dest="ckpt_dir")
+parser.add_argument("--data_dir", default="./datasets",
+                    type=str, dest="data_dir")
+parser.add_argument("--ckpt_dir", default="./checkpoint",
+                    type=str, dest="ckpt_dir")
 parser.add_argument("--log_dir", default="./log", type=str, dest="log_dir")
-parser.add_argument("--result_dir", default="./result", type=str, dest="result_dir")
+parser.add_argument("--result_dir", default="./result",
+                    type=str, dest="result_dir")
 
 parser.add_argument("--mode", default="train", type=str, dest="mode")
-parser.add_argument("--train_continue", default="off", type=str, dest="train_continue")
+parser.add_argument("--train_continue", default="off",
+                    type=str, dest="train_continue")
 
 args = parser.parse_args()
 
-## 트레이닝 파라메터 설정하기
-lr = args.lr  # 학습횟수 
+# 트레이닝 파라메터 설정하기
+lr = args.lr  # 학습횟수
 batch_size = args.batch_size
 num_epoch = args.num_epoch
 
-data_dir = args.data_dir # 데이터셋 저장 디렉토리
+data_dir = args.data_dir  # 데이터셋 저장 디렉토리
 ckpt_dir = args.ckpt_dir
-log_dir = args.log_dir # tensorboard log 저장 디렉토리
+log_dir = args.log_dir  # tensorboard log 저장 디렉토리
 result_dir = args.result_dir
 
 mode = args.mode
 train_continue = args.train_continue
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # gpu 혹은 cpu에서 동작할 지 결정해줌
+device = torch.device('cuda' if torch.cuda.is_available()
+                      else 'cpu')  # gpu 혹은 cpu에서 동작할 지 결정해줌
 
 print("learning rate: %.4e" % lr)
 print("batch size: %d" % batch_size)
@@ -63,20 +71,25 @@ print("log dir: %s" % log_dir)
 print("result dir: %s" % result_dir)
 print("mode: %s" % mode)
 
-## 디렉토리 생성하기
+# 디렉토리 생성하기
 if not os.path.exists(result_dir):
     os.makedirs(os.path.join(result_dir, 'png'))
     os.makedirs(os.path.join(result_dir, 'numpy'))
 
-## 네트워크 학습하기
+# 네트워크 학습하기
 if mode == 'train':
-    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
+    transform = transforms.Compose(
+        [Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
 
-    dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
-    loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
+    dataset_train = Dataset(data_dir=os.path.join(
+        data_dir, 'train'), transform=transform)
+    loader_train = DataLoader(
+        dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
 
-    dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
-    loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=False, num_workers=0)
+    dataset_val = Dataset(data_dir=os.path.join(
+        data_dir, 'val'), transform=transform)
+    loader_val = DataLoader(
+        dataset_val, batch_size=batch_size, shuffle=False, num_workers=0)
 
     # 그밖에 부수적인 variables 설정하기
     num_data_train = len(dataset_train)
@@ -85,35 +98,41 @@ if mode == 'train':
     num_batch_train = np.ceil(num_data_train / batch_size)
     num_batch_val = np.ceil(num_data_val / batch_size)
 else:
-    transform = transforms.Compose([Normalization(mean=0.5, std=0.5), ToTensor()])
+    transform = transforms.Compose(
+        [Normalization(mean=0.5, std=0.5), ToTensor()])
 
-    dataset_test = Dataset(data_dir=os.path.join(data_dir, 'test'), transform=transform)
-    loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=0)
+    dataset_test = Dataset(data_dir=os.path.join(
+        data_dir, 'test'), transform=transform)
+    loader_test = DataLoader(
+        dataset_test, batch_size=batch_size, shuffle=False, num_workers=0)
 
     # 그밖에 부수적인 variables 설정하기
     num_data_test = len(dataset_test)
 
     num_batch_test = np.ceil(num_data_test / batch_size)
 
-## 네트워크 생성하기
+# 네트워크 생성하기
 net = UNet().to(device)
 
-## 손실함수 정의하기
+# 손실함수 정의하기
 fn_loss = nn.BCEWithLogitsLoss().to(device)
 
-## Optimizer 설정하기
+# Optimizer 설정하기
 optim = torch.optim.Adam(net.parameters(), lr=lr)
 
-## 그밖에 부수적인 functions 설정하기
-fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
-fn_denorm = lambda x, mean, std: (x * std) + mean
-fn_class = lambda x: 1.0 * (x > 0.5)
+# 그밖에 부수적인 functions 설정하기
 
-## Tensorboard 를 사용하기 위한 SummaryWriter 설정
+
+def fn_tonumpy(x): return x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+def fn_denorm(x, mean, std): return (x * std) + mean
+def fn_class(x): return 1.0 * (x > 0.5)
+
+
+# Tensorboard 를 사용하기 위한 SummaryWriter 설정
 writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
 writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
 
-## 네트워크 학습시키기
+# 네트워크 학습시키기
 st_epoch = 0
 
 # TRAIN MODE
@@ -143,17 +162,20 @@ if mode == 'train':
             # 손실함수 계산
             loss_arr += [loss.item()]
 
-            print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
-                  (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
-
             # Tensorboard 저장하기
             label = fn_tonumpy(label)
             input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
             output = fn_tonumpy(fn_class(output))
 
-            writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
-            writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
-            writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f | IoU %.4f" %
+                  (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr), iou_numpy(output, label)))
+
+            writer_train.add_image(
+                'label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            writer_train.add_image(
+                'input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            writer_train.add_image(
+                'output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
 
         writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
 
@@ -173,21 +195,24 @@ if mode == 'train':
 
                 loss_arr += [loss.item()]
 
-                print("VALID: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
-                      (epoch, num_epoch, batch, num_batch_val, np.mean(loss_arr)))
-
                 # Tensorboard 저장하기
                 label = fn_tonumpy(label)
                 input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
                 output = fn_tonumpy(fn_class(output))
 
-                writer_val.add_image('label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
-                writer_val.add_image('input', input, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
-                writer_val.add_image('output', output, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+                print("VALID: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f | IoU %.4f" %
+                      (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr), iou_numpy(output, label)))
+
+                writer_val.add_image(
+                    'label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+                writer_val.add_image(
+                    'input', input, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+                writer_val.add_image(
+                    'output', output, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
 
         writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
 
-        if epoch % 50 == 0:
+        if epoch == num_epoch:
             save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
 
     writer_train.close()
@@ -213,8 +238,8 @@ else:
 
             loss_arr += [loss.item()]
 
-            print("TEST: BATCH %04d / %04d | LOSS %.4f" %
-                  (batch, num_batch_test, np.mean(loss_arr)))
+            print("TEST: BATCH %04d / %04d | LOSS %.4f | IoU %.4f" %
+                  (batch, num_batch_test, np.mean(loss_arr), iou_numpy(output, label)))
 
             # Tensorboard 저장하기
             label = fn_tonumpy(label)
@@ -224,14 +249,19 @@ else:
             for j in range(label.shape[0]):
                 id = num_batch_test * (batch - 1) + j
 
-                plt.imsave(os.path.join(result_dir, 'png', 'label_%04d.png' % id), label[j].squeeze(), cmap='gray')
-                plt.imsave(os.path.join(result_dir, 'png', 'input_%04d.png' % id), input[j].squeeze(), cmap='gray')
-                plt.imsave(os.path.join(result_dir, 'png', 'output_%04d.png' % id), output[j].squeeze(), cmap='gray')
+                plt.imsave(os.path.join(result_dir, 'png', 'label_%04d.png' %
+                           id), label[j].squeeze(), cmap='gray')
+                plt.imsave(os.path.join(result_dir, 'png', 'input_%04d.png' %
+                           id), input[j].squeeze(), cmap='gray')
+                plt.imsave(os.path.join(result_dir, 'png', 'output_%04d.png' %
+                           id), output[j].squeeze(), cmap='gray')
 
-                np.save(os.path.join(result_dir, 'numpy', 'label_%04d.npy' % id), label[j].squeeze())
-                np.save(os.path.join(result_dir, 'numpy', 'input_%04d.npy' % id), input[j].squeeze())
-                np.save(os.path.join(result_dir, 'numpy', 'output_%04d.npy' % id), output[j].squeeze())
+                np.save(os.path.join(result_dir, 'numpy',
+                        'label_%04d.npy' % id), label[j].squeeze())
+                np.save(os.path.join(result_dir, 'numpy',
+                        'input_%04d.npy' % id), input[j].squeeze())
+                np.save(os.path.join(result_dir, 'numpy',
+                        'output_%04d.npy' % id), output[j].squeeze())
 
     print("AVERAGE TEST: BATCH %04d / %04d | LOSS %.4f" %
           (batch, num_batch_test, np.mean(loss_arr)))
-
